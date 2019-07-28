fc07e24ce3f9c65b8c22eab281504f3efebe95ac
Diego Londono <deveplon@gmail.com>
2019-07-28 20:41:38 -0300

    changes on footer

--- components/global/Footer.vue
+++ components/global/Footer.vue
@@ -8,9 +8,16 @@
 footer {
   position: relative;
   bottom: 0;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
   width: 100%;
   height: 40px;
+  padding: 0 50px;
   border-top: 1px solid var(--border-and-shadow);
   background-color: var(--bg-light);
+}
+footer span {
+  font-size: 0.9rem;
 }
 </style>
